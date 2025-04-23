74682a34e98581d77447a3dda78cbc9e7fc1ef42
joaoguzela <joao.guzela@gmail.com>
2025-04-22 21:53:03 -0300

    feat: implement Prisma repository interfaces for brands, products, and users

---
+++ src/http/repositories/productsRepository.ts
@@ -0,0 +1,34 @@
+import { Product, Prisma } from '@prisma/client';
+import { prisma } from '@/providers/prisma';
+import { IProductsRepository } from '../repositories/interfaces/iProductsRepository';
+
+export class PrismaProductsRepository implements IProductsRepository {
+  async create(data: Prisma.ProductCreateInput): Promise<Product> {
+    return prisma.product.create({ data });
+  }
+
+  async findAll(
+    page: number = 1,
+    limit: number = 10,
+    nameFilter?: string,
+  ): Promise<Product[]> {
+    return prisma.product.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      where: nameFilter
+        ? { name: { contains: nameFilter, mode: 'insensitive' } }
+        : {},
+      include: {
+        brand: true,
+      },
+    });
+  }
+
+  async count(nameFilter?: string): Promise<number> {
+    return prisma.product.count({
+      where: nameFilter
+        ? { name: { contains: nameFilter, mode: 'insensitive' } }
+        : {},
+    });
+  }
+}
